ebfef94f25ef5c3893ecdccd5b2b214ff85eae39
HaRuMorning <HaRuMorning@gmail.com>
2015-03-17 11:07:13 +0900

    hrJqueryAnularJs0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # HrJqueryAngularJs
 Jquery + AngularJs ( Directive )
+
+
+##Directive Manual
+
+###hrJqDatepicker
+ex1 ) <input type="text" hr-jq-datepicker="yy-mm-dd" ng-model="modeltest" />
+ex2 ) <input type="text" hr-jq-datepicker ng-model="modeltest" />
+ex3 ) <input type="text" hr-jq-datepicker="yy-mm-dd" />
+
+ex4 ) <input type="text" hr-jq-datepicker="{
+			dateFormat:'yy/mm/dd',
+			onSelect:function(date){ 
+				alert(date); 
+			}
+		}" />
+
+
+###Auto Jquery Sync
+해당 JS적용 후 실행시 데이터 싱크 자동맞춤 --> $("#sample").val("sample");
+
+
--- js/jquery-angularjs-hr-0.0.1.js
+++ js/jquery-angularjs-hr-0.0.1.js
@@ -1,50 +1,111 @@
 $.fn.hrAngularJs = (function($){
 	var options = {
 		ngApp:null
-		,ngAppName:"NgAppModule"
+		,ngAppName:"$$HrNgAppModule"
+	}
+
+	var getHrCount = function(){
+		if(!$.$$HrCnt){
+			$.$$HrCnt = 1;
+		} else {
+			$.$$HrCnt++;
+		}
+		return $.$$HrCnt;
+	}
+
+	var setHrDirectiveSetting = function(ngModual){
+		
+		ngModual.directive('hrJqDatepicker', function () {
+			return {
+				restrict: 'A',
+				require : '^?ngModel',
+				transclude: true,
+				replace: true,
+				link: function (scope, element, attrs, ngModelCtrl) {
+					
+					var options = null;
+					var onSelect = null;
+
+					var settingModel = function(date,ngModelCtrl){
+						if(ngModelCtrl){
+							ngModelCtrl.$setViewValue(date);
+							ngModelCtrl.$render();
+						}
+					}
+
+					try{
+						options = eval("("+attrs.hrJqDatepicker+")");
+						if(options.onSelect) { onSelect = options.onSelect;};
+						if(onSelect){
+							options.onSelect = function(date){
+								onSelect(date);
+								settingModel(date,ngModelCtrl);
+							}
+						} else {
+							options.onSelect = function(date){
+								settingModel(date,ngModelCtrl);
+							}
+						}
+					}
+					catch(e){
+						options = {
+							dateFormat:attrs.hrJqDatepicker
+							,onSelect:function(date){
+								settingModel(date,ngModelCtrl);
+							}
+						};
+					}
+
+					if(!options.dateFormat) options.dateFormat = "yy-mm-dd";
+					
+					$(element).datepicker(options);
+				}
+			};
+		});
+
 	}
 
 	var hrModule = function(ngOptions){
 
-		
 		var ngOptions = ngOptions || {};
-		
-		options.ngApp 		= this;
-		options.ngAppName 	= ngOptions.ngAppName || $(options.ngApp).attr("ng-app") || options.ngAppName;
 
-		if(!$(options.ngApp).attr("ng-app")){
-			$(options.ngApp).attr("ng-app",options.ngAppName);
+		ngOptions.ngApp 		= $(this).get(0);
+
+		if(!ngOptions.ngApp.controller && !ngOptions.ngApp.directive && !ngOptions.ngApp.config && !ngOptions.ngApp.run){
+			ngOptions.ngAppName 	= ngOptions.ngAppName || options.ngAppName+"_"+getHrCount();
+			ngOptions.ngApp = angular.module(ngOptions.ngAppName, []);
+			setHrDirectiveSetting(ngOptions.ngApp);
+			angular.bootstrap(this, [ngOptions.ngAppName]);
+		} else {
+			setHrDirectiveSetting(ngOptions.ngApp);
 		}
-		
-		(function(){
-			
-			angular.module(options.ngAppName, [])
-			
-			.directive('hrJqDatepicker', function () {
-			    return {
-			        restrict: 'A',
-			        link: function (scope, element, attrs, ngModelCtrl) {
-				        
-				        var dateFormat = attrs.hrJqDatepicker || "yy-mm-dd";
-				        
-			            $(element).datepicker({
-				            dateFormat:dateFormat
-					        ,onSelect:function(date){
-						        if(ngModelCtrl){
-						        	ngModelCtrl.$setViewValue(date);
-						        	ngModelCtrl.$render();
-						        }
-					        }
-					    });
-			        }
-			    };
-			});
-			
-		})();
 
+		$.valHooks.input = {
+			set: function( elem, value ) {
+				if(angular.element(elem).controller('ngModel')){
+					angular.element(elem).controller('ngModel').$setViewValue(value);
+				}
+			}
+		};
+
+		$.valHooks.textarea = {
+			set: function( elem, value ) {
+				if(angular.element(elem).controller('ngModel')){
+					angular.element(elem).controller('ngModel').$setViewValue(value);
+				}
+			}
+		};
 
 		
 	}
-
+/*
+	hrModule.val = function(val){
+		alert(this);
+		var ret_val;
+		ret_val = $(this).val(val);
+		$(this).trigger('input');
+		return ret_val;
+	}
+*/
 	return hrModule;
 })($);
